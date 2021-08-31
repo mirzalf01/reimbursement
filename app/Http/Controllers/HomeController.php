f0d75bb52894f079788528c7868b291440b598d9
mirzalf01 <irzalfahriza0@gmail.com>
2021-08-31 20:33:04 +0700

    Category chart in dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,15 +24,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $approved = Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Approved')->get();
-        $pending = Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Pending')->get();
-        $rejected = Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Rejected')->get();
-        $totalReimburse = Reimbursement::where('user_id', auth()->user()->id)->where('created_at', 'like', date("Y-m").'%')
-            ->where('status', 'Approved')
-            ->sum('amount');
+        $info = [
+            'approved' => count(Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Approved')->get()),
+            'pending' => count(Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Pending')->get()),
+            'rejected' => count(Reimbursement::where('user_id', auth()->user()->id)->where('status', 'Rejected')->get()),
+            'totalReimburse' => Reimbursement::where('user_id', auth()->user()->id)->where('created_at', 'like', date("Y-m").'%')
+                                    ->where('status', 'Approved')
+                                    ->sum('amount')
+        ];
+
+        $reimburseData = [
+            'travel' => count(Reimbursement::where('user_id', auth()->user()->id)->where('Category', 'Travel')->get()),
+            'fuel' => count(Reimbursement::where('user_id', auth()->user()->id)->where('Category', 'Fuel')->get()),
+            'medical' => count(Reimbursement::where('user_id', auth()->user()->id)->where('Category', 'Medical')->get()),
+            'accomodation' => count(Reimbursement::where('user_id', auth()->user()->id)->where('Category', 'Accomodation')->get()),
+            'other' => count(Reimbursement::where('user_id', auth()->user()->id)->where('Category', 'Other')->get())
+        ];
+
         $reimbursements = Reimbursement::select(['title', 'amount', 'status'])
             ->where('user_id', auth()->user()->id)
             ->latest()->take(5)->get();
-        return view('home', compact('reimbursements', 'approved', 'pending', 'rejected', 'totalReimburse'));
+        return view('home', compact('reimbursements', 'info', 'reimburseData'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                     <div class="info-box-content">
                         <span class="info-box-text">Approved</span>
                         <span class="info-box-number">
-                            {{ count($approved) }}
+                            {{ $info['approved'] }}
                         </span>
                     </div>
                     <!-- /.info-box-content -->
@@ -41,7 +41,7 @@
                         <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-clock"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Pending</span>
-                            <span class="info-box-number">{{ count($pending) }}</span>
+                            <span class="info-box-number">{{ $info['pending'] }}</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
@@ -57,7 +57,7 @@
                         <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-times-circle"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Rejected</span>
-                            <span class="info-box-number">{{ count($rejected) }}</span>
+                            <span class="info-box-number">{{ $info['rejected'] }}</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
@@ -71,7 +71,7 @@
                         <div class="info-box-content">
                             <span class="info-box-text">Total</span>
                             <span class="info-box-number">
-                                Rp. {{ number_format($totalReimburse, 0, ".", ".") }}
+                                Rp. {{ number_format($info['totalReimburse']) }}
                             </span>
                         </div>
                         <!-- /.info-box-content -->
@@ -119,10 +119,57 @@
                         <!-- /.card-body -->
                     </div>
                 </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                          <h3 class="card-title">Category Chart</h3>
+                        </div>
+                        <div class="card-body">
+                          <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
             </div>
         </div>
     </section>
     {{-- Main Content --}}
     
 
+@endsection
+
+@section('js')
+    <!-- ChartJS -->
+    <script src="{{ asset('AdminLTE/plugins/chart.js/Chart.min.js') }}"></script>
+    <script>
+        $(function(){
+            var donutChartCanvas = $('#donutChart').get(0).getContext('2d');
+            var donutData        = {
+            labels: [
+                'Travel',
+                'Fuel',
+                'Medical',
+                'Accomodation',
+                'Other'
+            ],
+            datasets: [
+                {
+                data: [{{$reimburseData['travel']}},{{$reimburseData['fuel']}},{{$reimburseData['medical']}},{{$reimburseData['accomodation']}},{{$reimburseData['other']}}],
+                backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc'],
+                }
+            ]
+            }
+            var donutOptions     = {
+            maintainAspectRatio : false,
+            responsive : true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            var donutChart = new Chart(donutChartCanvas, {
+            type: 'doughnut',
+            data: donutData,
+            options: donutOptions
+            })
+        })
+    </script>
 @endsection
